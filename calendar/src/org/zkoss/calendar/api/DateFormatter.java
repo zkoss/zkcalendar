d98dc009f40401da43974457487d18a3458818d7
leonlee <leonlee@potix.com>
2021-01-07 12:55:52 +0800

    refine ZKCAL-73: modify Interface and java doc

--- calendar/src/org/zkoss/calendar/api/DateFormatter.java
+++ calendar/src/org/zkoss/calendar/api/DateFormatter.java
@@ -29,43 +29,30 @@ import java.util.TimeZone;
 public interface DateFormatter {
 	/**
 	 * Returns the caption of the day of week.
-	 * @deprecated since 3.0.0
 	 */
-	@Deprecated
 	public String getCaptionByDayOfWeek(Date date, Locale locale, TimeZone timezone);
 
 	/**
 	 * Returns the caption of the time of day.
-	 * @deprecated since 3.0.0
 	 */
-	@Deprecated
 	public String getCaptionByTimeOfDay(Date date, Locale locale, TimeZone timezone);
 
 	/**
 	 * Returns the caption of the date.
-	 * @deprecated since 3.0.0
 	 */
-	@Deprecated
 	public String getCaptionByDate(Date date, Locale locale, TimeZone timezone);
-
 	/**
 	 * Returns the caption of the date of month.
-	 * @deprecated since 3.0.0
 	 */
-	@Deprecated
 	public String getCaptionByDateOfMonth(Date date, Locale locale, TimeZone timezone);
 
 	/**
 	 * Returns the caption of the popup title.
-	 * @deprecated since 3.0.0
 	 */
-	@Deprecated
 	public String getCaptionByPopup(Date date, Locale locale, TimeZone timezone);
 
 	/**
 	 * Returns the caption of the week number within the current year.
-	 * @deprecated since 3.0.0
 	 */
-	@Deprecated
 	public String getCaptionByWeekOfYear(Date date, Locale locale, TimeZone timezone);
 }
--- calendar/src/org/zkoss/calendar/api/ZonedDateTimeFormatter.java
+++ calendar/src/org/zkoss/calendar/api/ZonedDateTimeFormatter.java
@@ -12,63 +12,87 @@ import java.util.TimeZone;
  *
  */
 public interface ZonedDateTimeFormatter extends DateFormatter {
+	/**
+	 * Returns the caption of the day of week.
+	 * @deprecated As of release 3.0.0
+	 */
+	@Deprecated
 	default public String getCaptionByDayOfWeek(Date date, Locale locale, TimeZone timezone) {
 		return getCaptionByDayOfWeek(ZonedDateTime.ofInstant(date.toInstant(), timezone.toZoneId()), locale);
 	}
 
 	/**
 	 * Returns the caption of the day of week.
-	 * @since 3.0.0
 	 */
 	public String getCaptionByDayOfWeek(ZonedDateTime date, Locale locale);
 
+	/**
+	 * Returns the caption of the time of day.
+	 * @deprecated As of release 3.0.0
+	 */
+	@Deprecated
 	default public String getCaptionByTimeOfDay(Date date, Locale locale, TimeZone timezone) {
 		return getCaptionByTimeOfDay(ZonedDateTime.ofInstant(date.toInstant(), timezone.toZoneId()), locale);
 	}
 
 	/**
 	 * Returns the caption of the time of day.
-	 * @since 3.0.0
 	 */
 	public String getCaptionByTimeOfDay(ZonedDateTime date, Locale locale);
 
+	/**
+	 * Returns the caption of the date.
+	 * @deprecated As of release 3.0.0
+	 */
+	@Deprecated
 	default public String getCaptionByDate(Date date, Locale locale, TimeZone timezone) {
 		return getCaptionByDate(ZonedDateTime.ofInstant(date.toInstant(), timezone.toZoneId()), locale);
 	}
 
 	/**
 	 * Returns the caption of the date.
-	 * @since 3.0.0
 	 */
 	public String getCaptionByDate(ZonedDateTime date, Locale locale);
 
+	/**
+	 * Returns the caption of the date of month.
+	 * @deprecated As of release 3.0.0
+	 */
+	@Deprecated
 	default public String getCaptionByDateOfMonth(Date date, Locale locale, TimeZone timezone) {
 		return getCaptionByDateOfMonth(ZonedDateTime.ofInstant(date.toInstant(), timezone.toZoneId()), locale);
 	}
 
 	/**
 	 * Returns the caption of the date of month.
-	 * @since 3.0.0
 	 */
 	public String getCaptionByDateOfMonth(ZonedDateTime date, Locale locale);
 
+	/**
+	 * Returns the caption of the popup title.
+	 * @deprecated As of release 3.0.0
+	 */
+	@Deprecated
 	default public String getCaptionByPopup(Date date, Locale locale, TimeZone timezone) {
 		return getCaptionByPopup(ZonedDateTime.ofInstant(date.toInstant(), timezone.toZoneId()), locale);
 	}
 
 	/**
 	 * Returns the caption of the popup title.
-	 * @since 3.0.0
 	 */
 	public String getCaptionByPopup(ZonedDateTime date, Locale locale);
 
+	/**
+	 * Returns the caption of the week number within the current year.
+	 * @deprecated As of release 3.0.0
+	 */
+	@Deprecated
 	default public String getCaptionByWeekOfYear(Date date, Locale locale, TimeZone timezone) {
 		return getCaptionByWeekOfYear(ZonedDateTime.ofInstant(date.toInstant(), timezone.toZoneId()), locale);
 	}
 
 	/**
 	 * Returns the caption of the week number within the current year.
-	 * @since 3.0.0
 	 */
 	public String getCaptionByWeekOfYear(ZonedDateTime date, Locale locale);
 }
